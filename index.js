e501156f9656f42d183a3816100a79ee0b565109
jenilshah93 <jenilshah93@gmail.com>
2018-11-30 20:16:14 -0800

    depth functionality

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-export default function flatten(arr) {
+export default function flatten(arr,depth=Infinity) {
+  if(!depth)
+    return arr
+   return depth_flat(arr,1)
+
+   
+  function depth_flat(arr,d){
     var res = []
     for(var item of arr){
-      if(Array.isArray(item))
-        res.push(...flatten(item))
+      if(Array.isArray(item) && d<depth)
+        res.push(...depth_flat(item,d+1))
       else
         res.push(item)
     }
     return res;
+  }
+ 
 }
+
